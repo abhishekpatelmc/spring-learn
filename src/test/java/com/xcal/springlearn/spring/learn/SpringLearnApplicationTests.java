c92b1f1542f9f6c80186956a98da0d80a219106a
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-09-06 22:27:10 -0400

    #1 First commit

---
+++ src/test/java/com/xcal/springlearn/spring/learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcal.springlearn.spring.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
